155c731881d9ce29b740734686bf34e45b6dfe8a
Olexandr Kovalyshyn <kovalyshyn@gmail.com>
2020-10-10 18:34:27 +0300

    Доповнє налаштуваня Webpack

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import name from "./index-2";
+import "./css/common.css";
+
+console.log(name);
+console.log("Це index.js");
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,32 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { Template } = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
     filename: "my-bundle.js",
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "src/index.html" }), new MiniCssExtractPlugin({})],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
   },
 };
